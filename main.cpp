b0ab6149e06c5e0c2e600224cde8ccac1b83b56a
rickyprophete <32998797+rickyprophete@users.noreply.github.com>
2021-01-11 18:01:10 -0500

    Check if integer repeats in a list of integers

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
 int main()
 {
-    int i,j,m,n,x,location;
-    cout <<"Enter a value to be searched: \n"; cin >> x;
-    int arr[]={1,2,3,4,5,6,7,8,9,10,11,22,33,44,55,66,77,88,99,111,222,333,444,555,666,777};
-    n = (sizeof(arr)/sizeof(arr[0]));
-    i=0,j = n-1;
-    while (i < j){
-        m = floor((i+j)/2);
-        if(x>arr[m]){
-            i = m + 1;
-        }else{j = m;}
+    int arr[5]={1,3,2,4,7};
+    for(int i(0);i<(sizeof(arr)/sizeof(arr[0]));i++){
+        for(int j=i+1;j<(sizeof(arr)/sizeof(arr[0]));j++){
+	        if((arr[i]==arr[j]) && (i != j)){
+	            cout << "Duplicate";
+	        } else {
+	        	cout<< "Unique";
+			}
+        }
     }
-    (x==arr[i])? location = i : location = NULL;
-    cout<<"Index of value in array is: "<<location;
 
-
-
+    return 0;
 }
